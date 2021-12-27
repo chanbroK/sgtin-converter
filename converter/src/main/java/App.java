c8bf3cc63c58d4450279d1216c41039f251cde8f
BeADia <koc0819@gmail.com>
2021-12-27 15:38:30 +0900

    load GCPPrefixList by JSONArray

---
+++ converter/src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class App {
+    public static void main(String args[]) throws IOException {
+        BarcodeToSgtin sgtin =  new BarcodeToSgtin();
+
+    }
+}
